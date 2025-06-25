e18ee7b80b5255775bcd37e58a5ee7c3ce03680d
Vlad <skodorvlad371@gmail.com>
2025-06-25 23:29:03 +0300

    'icons'

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -3,7 +3,22 @@ import Details from './details/Details'
 
 function MainPage() {
     return (
-        <div>
+        <div style={{position: 'relative'}}>
+            <div className="block-icon-edit">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_2097_42005)">
+                        <path d="M12 27.0002H6C5.73478 27.0002 5.48043 26.8948 5.29289 26.7073C5.10536 26.5197 5 26.2654 5 26.0002V20.4139C5.00012 20.149 5.10532 19.8951 5.2925 19.7077L20.7075 4.29266C20.895 4.10527 21.1493 4 21.4144 4C21.6795 4 21.9337 4.10527 22.1213 4.29266L27.7075 9.87516C27.8949 10.0627 28.0002 10.3169 28.0002 10.582C28.0002 10.8471 27.8949 11.1014 27.7075 11.2889L12 27.0002Z" stroke="#FCFCFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M27 27H12" stroke="#FCFCFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M17 8L24 15" stroke="#FCFCFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_2097_42005">
+                            <rect width="32" height="32" fill="white"/>
+                        </clipPath>
+                    </defs>
+                </svg>
+
+            </div>
             <div className="main-page-block" style={{
                 height: '309px',
                 backgroundImage: 'url(src/assets/babd01f0bff9b46fbc280f6d9640155dabf61483.jpg)',
--- src/pages/mainpage/details/Details.jsx
+++ src/pages/mainpage/details/Details.jsx
@@ -25,10 +25,25 @@ function Details() {
                         Add Name
                         <input type="text" placeholder="Enter name of your store" />
                     </label>
-                    <label>
+                    <label className="input-with-icon">
                         Add Website URL
-                        <input type="text" placeholder="exp.spirithero.com/abc-spirit-wear-store" />
+                        <div className="input-icon-wrapper">
+                            <input type="text" placeholder="exp.spirithero.com/abc-spirit-wear-store" />
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_4440_7186)">
+                                    <path d="M5.79313 13.5001H3C2.86739 13.5001 2.74021 13.4474 2.64645 13.3536C2.55268 13.2599 2.5 13.1327 2.5 13.0001V10.207C2.50006 10.0745 2.55266 9.94753 2.64625 9.85383L10.3538 2.14633C10.4475 2.05263 10.5746 2 10.7072 2C10.8397 2 10.9669 2.05263 11.0606 2.14633L13.8538 4.93758C13.9474 5.03134 14.0001 5.15847 14.0001 5.29102C14.0001 5.42357 13.9474 5.5507 13.8538 5.64446L6.14625 13.3538C6.05255 13.4474 5.92556 13.5 5.79313 13.5001Z"
+                                          stroke="#4E008E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M8.5 4L12 7.5" stroke="#4E008E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_4440_7186">
+                                        <rect width="16" height="16" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
                     </label>
+
 
                     <div className="social-inputs">
                         <label>
